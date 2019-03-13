d01d3e42e6675ec84fe0eab6a5c30d47102a495b
Richie Permana <richie.permana@gmail.com>
2019-03-13 12:36:28 +0800

    Removing index.php from url

--- application/config/config.php
+++ application/config/config.php
@@ -27,6 +27,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 */
+// $config['base_url'] = 'http://localhost/blanca/kp';
 $config['base_url'] = 'http://localhost/kominfo';
 /*
 |--------------------------------------------------------------------------
@@ -38,7 +39,7 @@ $config['base_url'] = 'http://localhost/kominfo';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
